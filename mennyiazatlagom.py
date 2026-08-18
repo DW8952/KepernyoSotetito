e09e9dd6cfbada14dd06306fc7a1e5adefb4fb10
DW8952 <szabolcs@gale.hu>
2026-08-19 00:19:18 +0200

    worked on atlag shi WIP

--- mennyiazatlagom.py
+++ mennyiazatlagom.py
@@ -12,3 +12,21 @@ if counts:
     print(f"A leggyakoribb szám: {most_common_number} (beirtad {frequency} alkalommal)")
 else:
     print("Nincs érvényes szám a fájlban.")   
+akartobbet = str(input("kell tobb info? Y/N"))
+if akartobbet == "Y" or akartobbet == "Yes":
+    mindenszam = []
+    with open("mentes.txt", "r") as file:
+        for line in file:
+            stripped = line.strip()
+            if stripped.isdigit():
+                szam = int(stripped)
+                if 0 <= number <= 100:
+                    hasznaltszam = szam
+                    if hasznaltszam in mindenszam:
+                        pass
+                    else:
+                        mindenszam.append(hasznaltszam)
+    print("Minden szám ami 0-100 között volt, és használtál:")
+    for i in range(mindenszam.len):
+        
+        print("")
